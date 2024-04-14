e4ff03e8f087ebbf291cafc38323b1ace1c4e396
Maciek Sitkowski <msitkowski94@gmail.com>
2024-04-14 17:08:42 +0200

    get bitbucket stats for PRs based on comments

---
+++ bitbucket.ts
@@ -0,0 +1,150 @@
+import {z} from 'zod'
+
+const configSchema = z.object({
+  auth: z.object({
+    username: z.string(),
+    password: z.string(),
+  }),
+  project: z.object({
+    workspace: z.string(),
+    repository: z.string(),
+  }),
+})
+
+type PaginatedResponse<T> = {
+  values: T[]
+  next?: string
+  page: number
+  pagelen: number
+}
+
+export class Bitbucket {
+  private username: string
+  private password: string
+  private workspace: string
+  private repository: string
+  private baseUrl: string
+
+  private bitbucketApiUrl = 'https://api.bitbucket.org/2.0'
+
+  constructor(config: {
+    auth: {username: string; password: string}
+    project: {workspace: string; repository: string}
+  }) {
+    const {auth, project} = configSchema.parse(config)
+
+    this.username = auth.username
+    this.password = auth.password
+    this.workspace = project.workspace
+    this.repository = project.repository
+    this.baseUrl = `${this.bitbucketApiUrl}/repositories/${this.workspace}/${this.repository}`
+  }
+
+  async getPullRequestComments(prId: number) {
+    type PullRequestComment = {
+      id: number
+      content: {
+        raw: string
+      }
+      user: {
+        display_name: string
+      }
+    }
+
+    const comments: PullRequestComment[] = []
+
+    const response = await this.get<PaginatedResponse<PullRequestComment>>(
+      `/pullrequests/${prId}/comments?pagelen=100`,
+    )
+
+    console.log(`Fetched ${response.values.length} comments`)
+
+    comments.push(...response.values)
+
+    let nextPageUrl = response.next
+
+    while (nextPageUrl) {
+      const nextResponse = await this.fetchWithAuth<
+        PaginatedResponse<PullRequestComment>
+      >(nextPageUrl)
+      comments.push(...nextResponse.values)
+
+      console.log(`Fetched ${nextResponse.values.length} comments`)
+
+      nextPageUrl = nextResponse.next
+    }
+
+    return comments
+  }
+
+  async getPullRequests({from}: {from: Date}) {
+    type PullRequest = {
+      author: {
+        display_name: string
+      }
+      id: number
+      [key: string]: any
+    }
+
+    const pullRequests: PullRequest[] = []
+
+    const response = await this.get<PaginatedResponse<PullRequest>>(
+      `/pullrequests?pagelen=50&q=created_on>=${from.toISOString()} AND comment_count>0`,
+    )
+
+    console.log(`Fetched ${response.values.length} pull requests`)
+
+    pullRequests.push(...response.values)
+
+    let nextPageUrl = response.next
+
+    while (nextPageUrl) {
+      const nextResponse = await this.fetchWithAuth<
+        PaginatedResponse<PullRequest>
+      >(nextPageUrl)
+      pullRequests.push(...nextResponse.values)
+
+      console.log(`Fetched ${nextResponse.values.length} pull requests`)
+
+      nextPageUrl = nextResponse.next
+    }
+
+    return pullRequests
+  }
+
+  async getCommits() {
+    return this.post(`/commits`)
+  }
+
+  async get<TResponse>(url: string, options: RequestInit = {}) {
+    return this.fetchWithAuth<TResponse>(`${this.baseUrl}${url}`, options)
+  }
+
+  async post<TResponse>(url: string, options: RequestInit = {}) {
+    return this.fetchWithAuth<TResponse>(`${this.baseUrl}${url}`, {
+      ...options,
+      method: 'POST',
+    })
+  }
+
+  async fetchWithAuth<TResponse>(url: string, options: RequestInit = {}) {
+    const response = await fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Accept: 'application/json',
+        Authorization: `Basic ${btoa(`${this.username}:${this.password}`)}`,
+      },
+    })
+
+    if (!response.ok) {
+      const body = await response.text()
+
+      throw new Error(
+        `Failed to fetch ${url}: ${response.status} ${response.statusText}\n${body}`,
+      )
+    }
+
+    return (await response.json()) as TResponse
+  }
+}
---
+++ index.ts
@@ -0,0 +1,64 @@
+import {subDays, subMonths} from 'date-fns'
+import {Bitbucket} from './bitbucket'
+
+const bitbucket = new Bitbucket({
+  auth: {
+    username: Bun.env.BITBUCKET_USERNAME!,
+    password: Bun.env.BITBUCKET_APP_PASSWORD!,
+  },
+  project: {
+    workspace: Bun.env.BITBUCKET_WORKSPACE!,
+    repository: Bun.env.BITBUCKET_REPOSITORY!,
+  },
+})
+
+const pullRequests = await bitbucket.getPullRequests({
+  from: subDays(new Date(), 90),
+})
+
+const users = new Map<
+  string,
+  {
+    comments: number
+    commentsLength: number
+  }
+>()
+
+for (const pullRequest of pullRequests) {
+  if (!users.has(pullRequest.author.display_name)) {
+    initializeUser(pullRequest.author.display_name)
+  }
+
+  const comments = await bitbucket.getPullRequestComments(pullRequest.id)
+
+  for (const comment of comments) {
+    const isReviewerComment =
+      comment.user.display_name !== pullRequest.author.display_name
+
+    if (isReviewerComment) {
+      if (!users.has(comment.user.display_name)) {
+        initializeUser(comment.user.display_name)
+      } else {
+        users.get(comment.user.display_name)!.comments++
+        users.get(comment.user.display_name)!.commentsLength +=
+          comment.content.raw.length
+      }
+    }
+  }
+}
+
+function initializeUser(name: string) {
+  users.set(name, {
+    comments: 0,
+    commentsLength: 0,
+  })
+}
+
+const usersArray = Array.from(users.entries())
+  .map(([name, stats]) => ({
+    name,
+    ...stats,
+  }))
+  .toSorted((a, b) => b.comments - a.comments)
+
+console.table(usersArray)
