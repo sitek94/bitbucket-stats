6a193c1622c591dcb277a5317dcb53cdb76b977e
Maciek Sitkowski <msitkowski94@gmail.com>
2024-11-08 18:51:22 +0100

    check if comment/approve is not on user's own pr

--- scripts/seed-db.ts
+++ scripts/seed-db.ts
@@ -12,19 +12,12 @@ import {
 import { OraLogger } from '@/logger/ora.logger'
 
 const logger = new OraLogger()
-const bitbucket = new Bitbucket({
-  auth: {
-    username: process.env.BITBUCKET_USERNAME!,
-    password: process.env.BITBUCKET_APP_PASSWORD!,
-  },
-  project: {
-    workspace: process.env.BITBUCKET_WORKSPACE!,
-    repository: process.env.BITBUCKET_REPOSITORY!,
-  },
-})
+const bitbucket = new Bitbucket()
+
+const from = subDays(new Date(), 312)
 
 await bitbucket.crawlPullRequests({
-  from: subDays(new Date(), 2),
+  queryOptions: { from },
   onBeforeProcessingPullRequest: async (pullRequest, index, total) => {
     const progress = `${index + 1}/${total}`
 
@@ -39,11 +32,7 @@ await bitbucket.crawlPullRequests({
     return { shouldProcess }
   },
   onAfterProcessingPullRequest: async (pullRequest, index, total) => {
-    await insertUser({
-      uuid: pullRequest.author.uuid,
-      display_name: pullRequest.author.display_name,
-    }).onConflictDoNothing()
-
+    await insertUser(pullRequest.author).onConflictDoNothing()
     await insertPullRequest({
       id: pullRequest.id,
       title: pullRequest.title,
@@ -55,33 +44,33 @@ await bitbucket.crawlPullRequests({
     }).onConflictDoNothing()
 
     for (const participant of pullRequest.participants) {
-      await insertUser({
-        uuid: participant.user.uuid,
-        display_name: participant.user.display_name,
-      }).onConflictDoNothing()
+      await insertUser(participant.user).onConflictDoNothing()
 
-      await insertPullRequestParticipant({
-        pull_request_id: pullRequest.id,
-        user_id: participant.user.uuid,
-        role: participant.role,
-        approved: participant.approved,
-      }).onConflictDoNothing()
+      const isOwner = participant.user.uuid === pullRequest.author.uuid
+      if (!isOwner) {
+        await insertPullRequestParticipant({
+          pull_request_id: pullRequest.id,
+          user_id: participant.user.uuid,
+          role: participant.role,
+          approved: participant.approved,
+        }).onConflictDoNothing()
+      }
     }
 
     for (const comment of pullRequest.comments) {
-      await insertUser({
-        uuid: comment.user.uuid,
-        display_name: comment.user.display_name,
-      }).onConflictDoNothing()
+      await insertUser(comment.user).onConflictDoNothing()
 
-      await insertPullRequestComment({
-        id: comment.id,
-        pull_request_id: pullRequest.id,
-        author_id: comment.user.uuid,
-        created_at: comment.created_on,
-        updated_at: comment.updated_on,
-        content: comment.content.raw,
-      })
+      const isOwner = comment.user.uuid === pullRequest.author.uuid
+      if (!isOwner) {
+        await insertPullRequestComment({
+          id: comment.id,
+          pull_request_id: pullRequest.id,
+          author_id: comment.user.uuid,
+          created_at: comment.created_on,
+          updated_at: comment.updated_on,
+          content: comment.content.raw,
+        }).onConflictDoNothing()
+      }
     }
 
     logger.stopLoading()
